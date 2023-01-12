5b64f4e1d9a8ef738d6c99fc040742015f42ffed
lukyrasocha <rasocha.lukas@gmail.com>
2023-01-12 15:46:39 +0100

    Working version of cave generation

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,54 +1,102 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System;
 
-public class MapGenerator : MonoBehaviour
-{
-    int[,] map;
+public class MapGenerator : MonoBehaviour {
 
-    public int width;
-    public int height;
-    public string seed;
-    private System.Random pseudoRandom;
+	public int width;
+	public int height;
 
-    [Range(0,100)]
-    public int fillPercentage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        GenerateMap();
-        
-    }
+	public string seed;
+	public bool useRandomSeed;
 
-    void GenerateMap(){
-        map = new int[width, height];
-        seed = (seed.Length <= 0) ? Time.time.ToString() : seed;
-        pseudoRandom = new System.Random(seed.GetHashCode());
+	[Range(0,100)]
+	public int randomFillPercent;
 
-        for (int x = 0; x < width; x++){
-            for (int y = 0; y < height; y++){
-                map[x,y] = (pseudoRandom.Next(0,100) > fillPercentage) ? 0 : 1;          // E.g. If you want 30 % of the map to be walls you need to do random > 0.7
-            }
-        }
-    }
+	int[,] map;
 
-    void OnDrawGizmos(){
-        if (map != null){
-            for (int x = 0; x < width; x++){
-                for (int y = 0; y < height; y++){
-                    Gizmos.color = (map[x,y] == 1)?Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width/2 + x + .5f, 0, -height/2 + y + .5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
+	void Start() {
+		GenerateMap();
+	}
 
-        }
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			GenerateMap();
+		}
+	}
 
-    }
+	void GenerateMap() {
+		map = new int[width,height];
+		RandomFillMap();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		for (int i = 0; i < 5; i ++) {
+			SmoothMap();
+		}
+	}
+
+
+	void RandomFillMap() {
+		if (useRandomSeed) {
+			seed = Time.time.ToString();
+		}
+
+		System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+		for (int x = 0; x < width; x ++) {
+			for (int y = 0; y < height; y ++) {
+				if (x == 0 || x == width-1 || y == 0 || y == height -1) {
+					map[x,y] = 1;
+				}
+				else {
+					map[x,y] = (pseudoRandom.Next(0,100) < randomFillPercent)? 1: 0;
+				}
+			}
+		}
+	}
+
+	void SmoothMap() {
+		for (int x = 0; x < width; x ++) {
+			for (int y = 0; y < height; y ++) {
+				int neighbourWallTiles = GetSurroundingWallCount(x,y);
+
+				if (neighbourWallTiles > 4)
+					map[x,y] = 1;
+				else if (neighbourWallTiles < 4)
+					map[x,y] = 0;
+
+			}
+		}
+	}
+
+	int GetSurroundingWallCount(int gridX, int gridY) {
+		int wallCount = 0;
+		for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX ++) {
+			for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY ++) {
+				if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height) {
+					if (neighbourX != gridX || neighbourY != gridY) {
+						wallCount += map[neighbourX,neighbourY];
+					}
+				}
+				else {
+					wallCount ++;
+				}
+			}
+		}
+
+		return wallCount;
+	}
+
+
+	void OnDrawGizmos() {
+		if (map != null) {
+			for (int x = 0; x < width; x ++) {
+				for (int y = 0; y < height; y ++) {
+					Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
+					Vector3 pos = new Vector3(-width/2 + x + .5f,0, -height/2 + y+.5f);
+					Gizmos.DrawCube(pos,Vector3.one);
+				}
+			}
+		}
+	}
+
 }
